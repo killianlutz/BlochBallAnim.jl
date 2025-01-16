cbb4794f412ce5362851f5da16173c8c141d56f9
KLutz <klutz.math@outlook.com>
2025-01-16 17:11:17 +0100

    up to date

--- scripts/run_animation.jl
+++ scripts/run_animation.jl
@@ -10,9 +10,9 @@ import GLMakie: theme_dark, Theme, with_theme
 figure_theme = Theme();
 
 ############ CHOOSE specific GKSL ODE parameters
-free_hamiltonian = [-1 2; 2 1] # hermitian
+free_hamiltonian = [-1 2; 2 1.0] # hermitian
 collapse_operators = [
-    [0 sqrt(3); 0 0], [0 0; 1 0]
+    [0 sqrt(3); 0 0], [0 0; 1 0.0]
 ] # any number of 2x2 matrices, unit damping rates
 
 ############ Run animation
